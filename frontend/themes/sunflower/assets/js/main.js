1c2cda5babc05550113b35c55befcf6462268485
chaxiaomao <1145470390@qq.com>
2019-04-09 00:27:01 +0800

    1

--- frontend/themes/sunflower/assets/js/main.js
+++
@@ -1,7 +0,0 @@
-$('#gen').click(function generate() {
-    $.post('/site/recommend-code-captcha', function(data) {
-        if (data) {
-            $('#recommend-broad').html(data.data)
-        }
-    });
-});

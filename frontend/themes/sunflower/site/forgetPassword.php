482ef5cd8e8ae8e8e64c8de0da09ee45c1268d46
chaxiaomao <1145470390@qq.com>
2019-05-23 14:12:33 +0800

    1

---
+++ frontend/themes/sunflower/site/forgetPassword.php
@@ -0,0 +1,95 @@
+<?php
+
+/* @var $this yii\web\View */
+/* @var $form yii\bootstrap\ActiveForm */
+
+/* @var $model \frontend\models\SignupForm */
+
+use common\components\SmsCaptcha\Captcha;
+use yii\helpers\Html;
+use yii\bootstrap\ActiveForm;
+use kartik\builder\Form;
+
+$messageName = $model->getMessageName();
+$this->title = Yii::t('app.c2', 'Forget Password');
+?>
+
+<div class="bg">
+    <p class="welcome"><?= Yii::t('app.c2', 'Welcome to signup') ?></p>
+</div>
+<div class="container">
+
+    <div id="tip_body" style="display: none;" class="alert alert-danger alert-dismissible" role="alert">
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span
+                    aria-hidden="true">&times;</span></button>
+        <p id="tip_content"></p>
+    </div>
+
+    <?php
+    $form = \kartik\widgets\ActiveForm::begin([
+        'options' => [
+            'id' => $model->getBaseFormName(),
+            'data-pjax' => false,
+            // 'class' => 'form-horizontal'
+        ]]);
+    ?>
+
+    <?php
+
+    echo Form::widget([
+        'model' => $model,
+        'form' => $form,
+        'columns' => 1,
+        'attributes' => [
+            'mobile_number' => [
+                'type' => Form::INPUT_TEXT,
+                'label' => false,
+                'fieldConfig' => [
+                    // 'template' => '<label for="signupform-mobile_number" class="col-xs-2 control-label">' . $model->getAttributeLabel('mobile_number') . '</label>
+                    //                 <div class="col-xs-10">{input}</div>',
+                    // 'options' => ['class' => 'form-group row']
+                ],
+                'options' => [
+                    'placeholder' => $model->getAttributeLabel('mobile_number'),
+                    // 'class' => 'form-control-lg'
+                ]
+            ],
+            'password' => [
+                'type' => Form::INPUT_PASSWORD,
+                'label' => false,
+                'options' => [
+                    'placeholder' => $model->getAttributeLabel('password'),
+                ]
+            ],
+            'passwordTwice' => [
+                'type' => Form::INPUT_PASSWORD,
+                'label' => false,
+                'options' => [
+                    'placeholder' => $model->getAttributeLabel('passwordTwice'),
+                ]
+            ],
+            'verifyCode' => [
+                'type' => Form::INPUT_WIDGET,
+                'widgetClass' => Captcha::className(),
+                'label' => false,
+                'options' => [
+                    'form' => $form,
+                    'mobileId' => Html::getInputId($model, 'mobile_number'),
+                ],
+            ],
+
+        ]
+    ]);
+
+
+    ?>
+
+    <div class="container tr">
+        <?php echo Html::submitButton(Yii::t('app.c2', 'Save'), ['class' => 'btn btn-warning btn-block font-white mb10']) ?>
+    </div>
+
+
+    <?php
+    \kartik\widgets\ActiveForm::end();
+    ?>
+</div>
